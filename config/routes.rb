21266e2a19f4a117002275a175b6bcb4a3f1a763
Sebastian Senf <contact@mustardamus.com>
2009-12-15 18:16:43 +0100

    partials for header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => "posts"
   
+  map.blog "blog", :controller => "posts"
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
