5a2946f38cd26fb455b553972521042b8299ce98
Sebastian Senf <contact@mustardamus.com>
2009-12-16 23:35:53 +0100

    wired up habtm association between posts and (post)categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   has_many :post_categorizations
-  has_many :post_categories, :through => :post_categorizations
+  has_many :categories, :through => :post_categorizations, :source => :post_category
 end
